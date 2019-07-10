377fec996777f083f09057857c0fed5b600b550d
godbasin <wangbeishan@163.com>
2019-07-10 21:09:11 +0800

    update

---
+++ 2/README.md
@@ -0,0 +1,17 @@
+# 文章地址
+[《9102全员学Vue--2.怎么三两下拼出一个页面》]()
+
+## Project setup
+```
+# 安装依赖
+npm run install
+
+# 本地启动项目
+npm run serve
+
+# 打包构建
+npm run build
+
+# lint 检查和处理
+npm run lint
+```
--- 2/src/App.vue
+++ 2/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,12 +16,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
 }
 </style>
